d9451e31fb2e5499d1026fde767303be2d08efdf
Bami <libamiew@gmail.com>
2021-05-23 22:27:58 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -236,53 +236,53 @@ jQuery(document).ready(function($) {
     /* ---------------------------------------------------------------------- */
 
 
-    var filterList = {
-        init: function() {
+    // var filterList = {
+    //     init: function() {
 
-            // MixItUp plugin
-            // http://mixitup.io
-            $('#portfoliolist').mixitup({
-                targetSelector: '.portfolio',
-                filterSelector: '.filter',
-                effects: ['fade'],
-                easing: 'snap',
-                // call the hover effect
-                onMixEnd: filterList.hoverEffect()
-            });
+    //         // MixItUp plugin
+    //         // http://mixitup.io
+    //         $('#portfoliolist').mixitup({
+    //             targetSelector: '.portfolio',
+    //             filterSelector: '.filter',
+    //             effects: ['fade'],
+    //             easing: 'snap',
+    //             // call the hover effect
+    //             onMixEnd: filterList.hoverEffect()
+    //         });
 
-        },
-        hoverEffect: function() {
+    //     },
+    //     hoverEffect: function() {
 
-            // Simple parallax effect
-            $('#portfoliolist .portfolio').hover(
-                    function() {
-                        $(this).find('.label').stop().animate({bottom: 0}, 200);
-                        $(this).find('img').stop().animate({top: -30}, 500);
-                    },
-                    function() {
-                        $(this).find('.label').stop().animate({bottom: -40}, 200);
-                        $(this).find('img').stop().animate({top: 0}, 300);
-                    }
-            );
+    //         // Simple parallax effect
+    //         $('#portfoliolist .portfolio').hover(
+    //                 function() {
+    //                     $(this).find('.label').stop().animate({bottom: 0}, 200);
+    //                     $(this).find('img').stop().animate({top: -30}, 500);
+    //                 },
+    //                 function() {
+    //                     $(this).find('.label').stop().animate({bottom: -40}, 200);
+    //                     $(this).find('img').stop().animate({top: 0}, 300);
+    //                 }
+    //         );
 
-        }
+    //     }
 
-    };
+    // };
 
-    // Run the show!
-    filterList.init();
+    // // Run the show!
+    // filterList.init();
 
-    /* ---------------------------------------------------------------------- */
-    /* ----------------------------- prettyPhoto ---------------------------- */
-    /* ---------------------------------------------------------------------- */
+    // /* ---------------------------------------------------------------------- */
+    // /* ----------------------------- prettyPhoto ---------------------------- */
+    // /* ---------------------------------------------------------------------- */
 
-    $("a[rel^='portfolio']").prettyPhoto({
-        animation_speed: 'fast', /* fast/slow/normal */
-        social_tools: '',
-        theme: 'pp_default',
-        horizontal_padding: 5,
-        deeplinking: false,
-    });
+    // $("a[rel^='portfolio']").prettyPhoto({
+    //     animation_speed: 'fast', /* fast/slow/normal */
+    //     social_tools: '',
+    //     theme: 'pp_default',
+    //     horizontal_padding: 5,
+    //     deeplinking: false,
+    // });
 
 
 
